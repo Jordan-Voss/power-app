af7703607bf1d9e907fb184afe453112aacacd8b
Jordan-Voss <jordan.voss2@mail.dcu.ie>
2022-04-11 23:47:35 +0100

    add drawer navigator to home screen

--- Navigation/AppNav.js
+++ Navigation/AppNav.js
@@ -1,18 +1,31 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import Home from '../Screens/Home'
+import {DrawerNavigator} from 'react-navigation'
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerContent from './DrawerContent'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const AppNav = createStackNavigator(
-  {
-    Home: { screen: Home },
-    NewReport: { screen: Home},
-    Reports: { screen: Home},
-    EditReport: { screen: Home}
-  },
-  {
-    initialRouteName: 'Home',
-    headerMode: 'none'
-
-  }
-)
-
-export default AppNav;
+const Drawer = createDrawerNavigator();
+export default function AppNav() {
+  return (
+      <NavigationContainer>
+      <Drawer.Navigator
+             drawerType="front"
+             initialRouteName="Profile"
+             drawerContentOptions={{
+               activeTintColor: '#e91e63',
+               itemStyle: { marginVertical: 10 },
+             }}
+      
+      >
+             {
+               DrawerContent.map(drawer=><Drawer.Screen
+                 key={drawer.name}
+                 name={drawer.name}
+                 component={Home}
+               />)
+             }
+      </Drawer.Navigator>
+      </NavigationContainer>
+       );
+      }
---
+++ Navigation/DrawerContent.js
@@ -0,0 +1,19 @@
+import Home from '../Screens/Home'
+
+export default [
+    {
+        name:'Home',
+        iconType:'Material',
+        iconName:'face-profile'
+    },
+    {
+        name:'ProfileScreen',
+        iconType:'Feather',
+        iconName:'settings'
+    },
+    {
+        name:'ReferScreen',
+        iconType:'Material',
+        iconName:'bookmark-check-outline'
+    },
+ ]
